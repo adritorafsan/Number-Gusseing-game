c959fcd157d01f59926192a683e9838467e2e741
adrito rafsan <adritorafsan@gmail.com>
2023-07-31 12:35:20 +0600

    yo

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-const gusses = Math.floor(Math.random()*10+1);
+const answer = Math.floor(Math.random()*10+1);
 let gussescount = 0;
+const inputbx = document.querySelector('#inputbx');
+const submitbtn = document.querySelector('.btn');
+const result = document.querySelector('.result');
+
+
+submitbtn.addEventListener("click",()=>{
+    
+    let gusse = inputbx.value;
+    gussescount+=1;
+    if(gusse == answer){
+        result.textContent=answer;
+        alert(`It tooks you ${gussescount}`)
+    }
+
+})
+
