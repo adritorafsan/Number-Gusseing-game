c7a324c70351ff19eb91ab6ce0628e075a102112
adrito rafsan <adritorafsan@gmail.com>
2023-08-01 11:26:48 +0600

    hu

---
+++ second/second.js
@@ -0,0 +1,6 @@
+const answer = Math.floor(Math.random()*10+1);
+let guesses = 0;
+const inputbx = document.querySelector('#inputbx');
+const btn = document.querySelector('.btn');
+const resultbx = document.querySelector('#resultbx');
+
