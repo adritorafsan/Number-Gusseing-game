458a9ff03597a26336bb17d833c609dc68016c2a
adrito rafsan <adritorafsan@gmail.com>
2023-08-01 11:32:15 +0600

    hu

--- second/second.js
+++ second/second.js
@@ -4,3 +4,16 @@ const inputbx = document.querySelector('#inputbx');
 const btn = document.querySelector('.btn');
 const resultbx = document.querySelector('#resultbx');
 
+btn.addEventListener('click',()=>{
+    guesses+=1;
+  let  userinput = inputbx.value;
+  if(userinput == answer){
+    resultbx.textContent = (`Succesful! The number is ${userinput} it Tooks you ${guesses} guesses`)
+  }
+  else if(userinput<answer){
+    alert('Too short');
+  }
+  else{
+    alert('Too large');
+  }
+})
