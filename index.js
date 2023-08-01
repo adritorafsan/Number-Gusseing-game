1c422f14bf2d00b78030ff5641f5a4668a7e062e
adrito rafsan <adritorafsan@gmail.com>
2023-08-01 10:37:47 +0600

    uuu

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let gussescount = 0;
 const inputbx = document.querySelector('#inputbx');
 const submitbtn = document.querySelector('.btn');
 const result = document.querySelector('.result');
+const Reload = document.querySelector('#Reload');
 
 
 submitbtn.addEventListener("click",()=>{
@@ -10,9 +11,20 @@ submitbtn.addEventListener("click",()=>{
     let gusse = inputbx.value;
     gussescount+=1;
     if(gusse == answer){
-        result.textContent=answer;
-        alert(`It tooks you ${gussescount}`)
+        
+        result.textContent= (`Result = the number is ${gusse} and It tooks you ${gussescount} gusses`);
+        
+    }
+    else if(gusse < answer){
+        alert('too small');
+    }
+    else{
+        alert('too large')
     }
 
 })
+
+Reload.onclick = ()=>{
+    
+}
 
